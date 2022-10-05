0acaceca3aef5e9a03c230b7be331b92164f4b79
DiegoP0805 <60204933+DiegoP0805@users.noreply.github.com>
2022-10-04 18:09:25 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from ray import RayTracer
+from material import Material
+from sphere import Sphere
+from color import Color
+from light import Light
+from lib import V3
+
+rubber = Material(diffuse=Color(255, 0, 0), albedo = [0.9, 0.1], spec = 10 )
+red_metal = Material(diffuse=Color(255, 0, 0), albedo = [0.8, 0.2], spec = 70)
+ivory = Material(diffuse=Color(255, 255, 255), albedo = [0.7, 0.3], spec = 50)
+white_cotton = Material(diffuse=Color(255, 255, 255), albedo = [0.9, 0.1], spec = 0)
+dark_brown_cotton = Material(diffuse=Color(181, 101, 29), albedo = [0.9, 0.1], spec = 0)
+light_brown_cotton = Material(diffuse=Color(200, 157, 124), albedo = [0.9, 0.1], spec = 0)
+black_metal = Material(diffuse=Color(0, 0, 0), albedo = [0.8, 0.2], spec = 70)
+
+r = RayTracer(500, 500)
+
+r.light = Light(V3(-20, 20, 20), 2, Color(255, 255, 255))
+r.scene = [
+    Sphere(V3(0, -1.5, -10), 1.5, ivory),
+    Sphere(V3(-2, -1, -12), 2, rubber),
+    Sphere(V3(1, 1, -8), 1.7, rubber),
+    Sphere(V3(-2, 2, -10), 2, ivory),
+]
+
+#r.light = Light(position=V3(0, 0, 0), intensity=1.4, color= Color(255, 255, 255))
+#r.scene = [
+#    #Sphere(V3(-2, 0, -10), 1, red_metal),
+#    #Sphere(V3(-3, 1, -10), 0.5, dark_brown_cotton),
+#    #Sphere(V3(-1, 1, -10), 0.5, dark_brown_cotton),
+#    #Sphere(V3(-3.25, -0.5, -10), 0.5, dark_brown_cotton),
+#    #Sphere(V3(-0.75, -0.5, -10), 0.5, dark_brown_cotton),
+#
+#    Sphere(V3(-2, -1.5, -10), 0.75, dark_brown_cotton),
+#    Sphere(V3(-2.625, -2.125, -10), 0.375, dark_brown_cotton),
+#    Sphere(V3(-1.375, -2.125, -10), 0.375, dark_brown_cotton),
+#    Sphere(V3(-1.6, -1.2, -8), 0.25, light_brown_cotton),
+#
+#    Sphere(V3(-1.4, -1.1, -7), 0.05, black_metal),
+#    Sphere(V3(-1.65, -1.2, -7), 0.05, black_metal),
+#    Sphere(V3(-1.15, -1.2, -7), 0.05, black_metal),
+#
+#
+#
+#    #Sphere(V3(2, 0, -10), 1, ivory),
+#    #Sphere(V3(3, 1, -10), 0.5, white_cotton),
+#    #Sphere(V3(1, 1, -10), 0.5, white_cotton),
+#    #Sphere(V3(3.25, -0.5, -10), 0.5, white_cotton),
+#    #Sphere(V3(0.75, -0.5, -10), 0.5, white_cotton),
+#    #Sphere(V3(2, -1.5, -10), 0.75, white_cotton),
+#    #Sphere(V3(2.625, -2.125, -10), 0.375, white_cotton),
+#    #Sphere(V3(1.375, -2.125, -10), 0.375, white_cotton),
+#    #Sphere(V3(1.6, -1.2, -8), 0.25, white_cotton),
+#    #Sphere(V3(1.4, -1.1, -7), 0.05, black_metal),
+#    #Sphere(V3(1.65, -1.2, -7), 0.05, black_metal),
+#    #Sphere(V3(1.15, -1.2, -7), 0.05, black_metal),
+#    ##prueba de material
+#]
+r.dense = 1
+r.render()
---
+++ ray.py
@@ -0,0 +1,135 @@
+from code import interact
+from lib import *
+import random
+from material import Material
+from sphere import Sphere
+from math import *
+from light import Light
+from color import Color
+
+class RayTracer(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.clear_color = Color(0, 0, 0)
+        self.current_color = Color(255,255, 255)
+        self.background_color = Color(0, 0, 0)
+        self.dense = 1
+        self.scene = []
+        self.light = None
+        self.clear()
+
+    def clear(self):
+        self.framebuffer = [
+            [self.clear_color for x in range(self.width)] 
+            for y in range(self.height)
+            ]
+
+    def point(self, x, y, c = None):
+        if y > 0 and y < self.height and x > 0 and x < self.width:
+            if c is None:
+                c = self.current_color
+            self.framebuffer[y][x] = c
+    
+    def write(self, filename):
+        writebmp(filename, self.width, self.height, self.framebuffer)
+
+    def color(self, r, g, b):
+        self.current_color = Color(r, g, b)    
+
+    def cast_ray(self, origin, direction):
+        material, intersect = self.scene_intersect(origin, direction)
+
+        if material is None:
+            return self.background_color
+        
+        l_dir = norm(sub(self.light.position, intersect.point))
+        diffuse_intensity = dot( l_dir, intersect.normal)
+        #print(material.diffuse[2], intensity)
+        if diffuse_intensity < 0:
+            return self.background_color
+        else:
+            shadow_bias = 1.1
+            shadow_origin = intersect.point + (intersect.normal * shadow_bias)
+            shadow_material, shadow_intersect = self.scene_intersect(shadow_origin, l_dir)
+            shadow_intensity = 1
+
+            if shadow_material:
+                shadow_intensity = 0.3
+
+
+
+            #diffuse = material.diffuse * diffuse_intensity
+            diffuse = material.diffuse * diffuse_intensity * material.albedo[0] * shadow_intensity
+            light_reflection = reflect(l_dir, intersect.normal)
+            reflection_intensity =  max(0, dot(light_reflection, direction))
+            specular_intensity = self.light.intensity * reflection_intensity**material.spec
+            specular = self.light.color * specular_intensity * material.albedo[1]
+
+            return diffuse + specular
+
+        #return material.diffuse
+        #
+        #for s in self.scene:
+        #    i = s.ray_intersect(origin, direction)
+        #    if i:
+        #        return s.color
+        #return self.background_color
+
+
+    def render(self):
+        fov = int(pi / 2)
+        aspect_ratio = self.width / self.height
+        #angle = tan(fov / 2)
+        angle = tan(fov / 2)
+        for y in range(self.height):
+            for x in range(self.width):
+                rand = random.uniform(0, 1)
+                if rand < self.dense:
+                    i = ((2 * (x + 0.5) / self.width) - 1) * angle * aspect_ratio
+                    j = (1 - 2 * (y + 0.5) / self.height) * angle 
+                    
+                    direction = V3(i, j, -1).norm()
+                    origin = V3(0, 0, 0)
+                    
+                    c = self.cast_ray(origin, direction)
+                    #c = color(255,0,0)
+                    self.point(x, y, c)
+                
+        self.write('ray.bmp')
+    
+    
+    def scene_intersect(self, origin, direction):
+        zbuffer = 999999
+        material = None
+        intersect = None
+    
+
+        for o in self.scene:
+            hit = o.ray_intersect(origin, direction)
+            if hit:
+                if hit.distance < zbuffer:
+                    zbuffer = hit.distance
+                    material = o.material
+                    intersect = hit
+        return material, intersect
+        #zbuffer = float('inf')
+        #material = None
+        #intersect = None
+        #
+        #for obj in self.scene:
+        #    hit = obj.ray_intersect(origin, direction)
+        #    if hit is not None:
+        #        if hit.distance < zbuffer:
+        #            zbuffer = hit.distance
+        #            material = obj.material
+        #            intersect = hit
+        #return material, intersect
+    #def raytrace(self, x, y):
+    #    return (0, 0, 0)
+
+#r = RayTracer(500, 500)
+#r.point(10, 10)
+#r.dense = 0.1
+#r.render()
+#
